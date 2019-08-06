8f3186840873cb78a366f5036029b5af418a0c7a
Anthony Molina <antonio-molina@att.net>
2019-08-06 21:02:30 +0000

    Automatically backed up by Learn

---
+++ lib/pet.rb
@@ -0,0 +1,35 @@
+class Cat
+  attr_reader :name, :owner
+  attr_accessor :mood
+  
+  class << self
+      def all
+      @all ||= [] # same as @all = @all || []
+    end
+    
+    def new(name, owner)
+      pet = super(name, owner)
+      all << pet
+      pet
+    end
+    
+    def with_owner(owner)
+      all.select { |pet| pet.owner == owner }
+    end
+  end
+  
+  def initialize(name, owner)
+    @name = name 
+    @mood = "nervous"
+    @owner = owner
+  end
+  
+  def sell
+    @mood = 'nervous'
+    @owner = nil
+  end
+  
+  def feed 
+    @mood = 'happy'
+  end
+end
