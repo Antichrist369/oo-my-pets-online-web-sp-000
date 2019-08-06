72c3b1fc454724bdeb8a4111faf9f81c8d013e45
Anthony Molina <antonio-molina@att.net>
2019-08-06 21:04:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,5 @@
-class Dog
-  class << self
-    def all
-      @all ||= [] # same as @all = @all || []
-    end
-    
-    def new(name, owner)
-      pet = super(name, owner)
-      all << pet
-      pet
-    end
-    
-    def with_owner(owner)
-      all.select { |pet| pet.owner == owner }
-    end
-  end
-  
-  attr_accessor :mood
-  attr_reader :name, :owner
-  
-  def initialize(name, owner)
-    @name = name 
-    @mood = "nervous"
-    @owner = owner
-  end 
-  
-  def sell
-    @mood = 'nervous'
-    @owner = nil
-  end
-  
+require_relative './pet'
+class Dog < Pet
   def walk
     @mood = 'happy'
   end
